b986a64fbcd10feaa784d0c16d8b1d7c9ba4c0d7
Pedro Paulo dos Santos <dr2p@hotmail.com>
2021-07-01 13:06:48 +0000

    ...

--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,13 @@ import { Application } from "https://deno.land/x/oak/mod.ts"
 import { organ as Morgan} from "https://raw.githubusercontent.com/denjucks/organ/master/mod.ts"
 import { Snelm as Helmet } from "https://deno.land/x/snelm/mod.ts";
 import { oakCors as Cors } from "https://deno.land/x/cors/mod.ts"
+import { router } from "./src/routes.ts";
+const app = new Application()
 
 
 
-require('dotenv').config()
+app.use(router.routes())
 
-const 
-    signUp = require('./src/routes'),
-    app = express()
-
-app
-    .use(morgan('dev'))
-    .use(helmet())
-    .use(express.json())
-    .use(cors())
-    .use('/', signUp)
+export {
+    app
+}
